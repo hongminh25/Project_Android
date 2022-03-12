078e49702615a350caecd419006a2976e564f675
HongMinh <dohongminh25@gmail.com>
2022-03-12 12:42:49 +0700

    update

--- app/src/main/java/com/example/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/app/ui/home/HomeFragment.java
@@ -1,13 +1,13 @@
 package com.example.app.ui.home;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 
